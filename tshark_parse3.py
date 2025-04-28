6adb1c8af4960a8d50edda7983bb80875e3d1c5f
gergese <jeffsong16@gmail.com>
2025-04-28 20:57:29 +0900

    entropy 제거

--- tshark_parse3.py
+++ tshark_parse3.py
@@ -31,8 +31,6 @@ def extract_conv(pcap_file):
         "-e", "tcp.dstport",
         "-e", "udp.dstport",
         "-e", "frame.len",
-        "-e", "tcp.payload",
-        "-e", "udp.payload",
         "-e", "_ws.col.Protocol",
         "-o", "nameres.mac_name:FALSE"
     ]
@@ -46,7 +44,7 @@ def extract_conv(pcap_file):
         with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as proc:
             for line in proc.stdout:
                 fields = line.strip().split('\t')
-                if len(fields) != 12:
+                if len(fields) != 10:
                     continue
 
                 src_ip = fields[0] if fields[0] else fields[1]
@@ -54,21 +52,21 @@ def extract_conv(pcap_file):
 
                 tcp_src, udp_src = fields[2], fields[3]
                 tcp_dst, udp_dst = fields[6], fields[7]
-                tcp_payload, udp_payload = fields[9], fields[10]
+                # tcp_payload, udp_payload = fields[9], fields[10]
 
                 try:
                     if tcp_src and tcp_dst:
                         src_port, dst_port = tcp_src, tcp_dst
                         layer = "tcp"
-                        binary_data = binascii.unhexlify(tcp_payload) if tcp_payload else b''
+                        # binary_data = binascii.unhexlify(tcp_payload) if tcp_payload else b''
                     elif udp_src and udp_dst:
                         src_port, dst_port = udp_src, udp_dst
                         layer = "udp"
-                        binary_data = binascii.unhexlify(udp_payload) if udp_payload else b''
+                        # binary_data = binascii.unhexlify(udp_payload) if udp_payload else b''
                     else:
                         continue
 
-                    entropy = calculate_entropy(binary_data)
+                    # entropy = calculate_entropy(binary_data)
 
                     conversation = {
                         "address_A": src_ip,
@@ -77,8 +75,8 @@ def extract_conv(pcap_file):
                         "port_B": int(dst_port),
                         "bytes": int(fields[8]),
                         "packets": 1,
-                        "protocol": fields[11],
-                        "entropy": entropy
+                        "protocol": fields[9],
+                        # "entropy": entropy
                     }
 
                     data[layer].append(conversation)
